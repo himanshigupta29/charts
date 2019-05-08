5f292bb584e347236389bb6dfc55f3cae33c0181
himanshi gupta <himanshigupta29@gmail.com>
2019-05-08 21:20:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const http = require('http');
+const express = require('express');
+
+const config = require('./config/env');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/static_global'));
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+app.get('/assignment', (req, res) => {
+	res.render('assignment');
+});
+
+app.listen(config.NODE_APP_PORT, () => {
+	console.log(`${config.APP_NAME} App listening on port ${config.NODE_APP_PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+## step 1
+npm install
+
+## step 2
+Run http://localhost:7000 on browser
+Run http://localhost:7000/assignment on browser
